32ad26f9b3c4fe4b1c30a9348225742bbe3dea3f
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2013-02-20 10:58:26 -0600

    Functioning dumper

--- dump.js
+++ dump.js
@@ -2,13 +2,73 @@
 "use strict";
 
 var options = {
-        mongoConnectionString: "mongodb://cdibox.volgy.com:27017/vu_cs103_trunc_d2",
+        mongoConnectionString: "mongodb://cdibox.volgy.com:27017/vu_cs103_trunc_d2"
     };
 
 var MongoClient = require('mongodb').MongoClient;
 
-var userColl, tweetColl, followColl;
+function sanitize(str) {
+    return str.replace(/\'/gi, "''").replace(/[\x00-\x20]/gi, " ");
+}
 
+function dump(userColl, tweetColl, followColl) {
+    var userCnt, tweetCnt;
+
+    userCnt = 1;
+    userColl.find().each(function (err, user) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        if (!user) {
+            console.warn("WARNING: empty user object");
+            return;
+        }
+        if ((userCnt % 1e3) === 0) {
+            console.warn("Processed %d users", userCnt);
+        }
+        user.seq = userCnt++;
+        user.id_str && console.log("users(%d).id = uint64(%s);", user.seq, user.id_str);
+        user.screen_name && console.log("users(%d).screen_name = '%s';", user.seq, user.screen_name);
+        user.name && console.log("users(%d).name = '%s';", user.seq, sanitize(user.name));
+        user.created_at && console.log("users(%d).created_at = '%s';", user.seq, user.created_at);
+        user.time_zone && console.log("users(%d).time_zone = '%s';", user.seq, user.time_zone);
+
+        followColl.find({src: user.id_str}).toArray(function (err, follows) {
+            follows.length && console.log("users(%d).follows = [%s];", user.seq, follows.map(function (f) {return "uint64(" + f.dst + ")";}).join(","));
+        });
+
+        followColl.find({dst: user.id_str}).toArray(function (err, followers) {
+            followers.length && console.log("users(%d).followers = [%s];", user.seq, followers.map(function (f) {return "uint64(" + f.src + ")";}).join(","));
+        });
+    });
+
+    tweetCnt = 1;
+    tweetColl.find().each(function (err, tweet) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        if (!tweet) {
+            console.warn("WARNING: empty tweet object");
+            return;
+        }
+        if ((tweetCnt % 10e3) === 0) {
+            console.warn("Processed %d tweets", tweetCnt);
+        }
+        tweet.seq = tweetCnt++;
+        tweet.id_str && console.log("tweets(%d).id = uint64(%s);", tweet.seq, tweet.id_str);
+        tweet.user && console.log("tweets(%d).user = uint64(%s);", tweet.seq, tweet.user.id_str);
+        tweet.created_at && console.log("tweets(%d).created_at = '%s';", tweet.seq, tweet.created_at);
+        tweet.text && console.log("tweets(%d).text = '%s';", tweet.seq, sanitize(tweet.text));
+        tweet.retweeted_status && console.log("tweets(%d).retweet_id = uint64(%s);", tweet.seq, tweet.retweeted_status.id_str);
+        tweet.entities && tweet.entities.hashtags && tweet.entities.hashtags.length && 
+            console.log("tweets(%d).hashtags = [%s];", tweet.seq, tweet.entities.hashtags.map(function (t) {return "'" + t.text + "'";}).join(","));
+        tweet.entities && tweet.entities.user_mentions && tweet.entities.user_mentions.length && 
+            console.log("tweets(%d).mentions = [%s];", tweet.seq, tweet.entities.user_mentions.map(function (m) {return "uint64(" + m.id_str + ")";}).join(","));
+        tweet.coordinates && console.log("tweets(%d).coordinates = [%s];", tweet.seq, tweet.coordinates.coordinates.toString());
+    });
+}
 
 // Connect to db, get all connections, start the crawler
 MongoClient.connect(options.mongoConnectionString, function (err, db) {
@@ -16,25 +76,22 @@ MongoClient.connect(options.mongoConnectionString, function (err, db) {
         console.error(err);
         return;
     }
-    db.collection("User", function (err, coll) {
+    db.collection("User", function (err, userColl) {
         if (err) {
             console.error(err);
             return;
         }
-        userColl = coll;
-        db.collection("Tweet", function (err, coll) {
+        db.collection("Tweet", function (err, tweetColl) {
             if (err) {
                 console.error(err);
                 return;
             }
-            tweetColl = coll;
-            db.collection("Follow", function (err, coll) {
+            db.collection("Follow", function (err, followColl) {
                 if (err) {
                     console.error(err);
                     return;
                 }
-                followColl = coll;
-                dump();
+                dump(userColl, tweetColl, followColl);
             });
         });
     });
